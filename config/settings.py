bc0b90dd3b9653db667ad19e80d8ce100db1be5e
zankoAn <zankoan4@gmail.com>
2024-06-28 12:51:40 +0330

    Set db number for redis

--- config/settings.py
+++ config/settings.py
@@ -99,5 +99,6 @@ CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.redis.RedisCache",
         "LOCATION": "redis://127.0.0.1:6379",
-    }
+    },
+    "OPTIONS": {"db": "3"},
 }
