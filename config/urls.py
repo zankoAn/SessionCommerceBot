2633305f2ef09906e0d83406a98c6cfe0ddd4860
zankoAn <zankoan4@gmail.com>
2024-07-03 18:09:32 +0330

    Add new url for payments and seperate the development urls

--- config/urls.py
+++ config/urls.py
@@ -7,6 +7,9 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('bot/', include('ecommerce.bot.urls')),
+    path('payment/', include('ecommerce.payment.urls')),
+]
 
-    # path('payment/', include('ecommerce.payment.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
