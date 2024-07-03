09b47666b905715bd21757c2f4efd8a9c64ef944
zankoAn <zankoan4@gmail.com>
2024-07-03 18:20:45 +0330

    Add new payment models and update the admin

--- ecommerce/payment/admin.py
+++ ecommerce/payment/admin.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
-from .models import *
+from .models import Transaction, CryptoPayment, ZarinPalPayment, PerfectMoneyPayment
 
 
-@admin.register(Payment)
-class PaymentAdmin(admin.ModelAdmin):
-    list_display = ("id", "amount", "is_paid")
-    list_editable = ("is_paid",)
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "payer",
+        "status",
+        "payment_method",
+        "amount_usd",
+        "amount_rial",
+    )
+    list_editable = ("status",)
+    list_display_links = ("payer", )
+
+
+@admin.register(CryptoPayment)
+class CryptoAdmin(admin.ModelAdmin):
+    list_display = ("id", "network", "currency", "payment_amount_coin")
+    list_display_links = ("id", "network", "currency")
+
+
+@admin.register(ZarinPalPayment)
+class ZarinPalAdmin(admin.ModelAdmin):
+    list_display = ("id", "transaction", "authority")
+    list_display_links = ("id", "transaction")
+
+
+@admin.register(PerfectMoneyPayment)
+class PerfectMoneyAdmin(admin.ModelAdmin):
+    list_display = ("id", "evoucher", "activation_code")
+    list_display_links = ("id", "evoucher", "activation_code")
--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,32 +1,164 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
+from django.utils.translation import gettext as __
 
 User = get_user_model()
 
 
-class Payment(models.Model):
+class Transaction(models.Model):
+    class StatusChoices(models.TextChoices):
+        PAID = __("Success Transaction ✅")
+        PAID_OVER = __("Success Transaction And Pay more ✅")
+        FAIL = __("Failed Transaction ❌")
+        IN_PROGRESS = __("⏳ In Progress")
+        WRONG_AMOUNT = ("Wrong Amount Waiting ❌")
+
+    class PaymentMethodChoices(models.TextChoices):
+        ZARINPAL = __("ZarinPal")
+        PERFECT_MONEY = __("PerfectMoney")
+        CRYPTO = __("Cryptocurrency")
+
     payer = models.ForeignKey(
         to=User,
         on_delete=models.SET_NULL,
-        related_name='payments',
+        related_name=_("payments"),
         null=True
     )
-    amount  = models.BigIntegerField(
-        default=0,
-        verbose_name='amount',
+    status = models.CharField(
+        max_length=50,
+        verbose_name=_("session status"),
+        choices=StatusChoices.choices,
+        default=StatusChoices.IN_PROGRESS,
     )
-    transaction_id = models.CharField(
-        max_length=70,
-        verbose_name="Transaction Id",
+    payment_method = models.CharField(
+        max_length=50,
+        verbose_name=_("payment method"),
+        choices=PaymentMethodChoices.choices,
+        default=PaymentMethodChoices.ZARINPAL,
     )
-    is_paid = models.BooleanField(
-        default=False,
-        verbose_name='Is Paid ?',
+    amount_rial = models.IntegerField(
+        null=True,
+        blank=True,
+        verbose_name=_("amount rial"),
+    )
+    amount_usd = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        null=True,
+        blank=True,
+        verbose_name=_("amount dollar"),
     )
     created = models.DateTimeField(
         auto_now_add=True,
-        verbose_name='payment creation time',
+        verbose_name=_("creation time"),
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True,
+        verbose_name=_("update time")
+    )
+
+    @property
+    def get_amount_rial(self):
+        return int(self.amount_rial)
+
+    def __str__(self) -> str:
+        return _("Transaction: {}").format(self.id)
+
+class ZarinPalPayment(models.Model):
+    transaction = models.OneToOneField(
+        to=Transaction,
+        on_delete=models.CASCADE,
+        verbose_name=_("related transaction"),
+        related_name="zarinpal"
+    )
+    authority = models.CharField(
+        max_length=70,
+        null=True,
+        blank=True,
+        unique=True,
+        verbose_name="authority id",
     )
 
     def __str__(self):
-        return str(self.payer)
+        return _("Authority: {}").format(self.authority)
+
+
+class PerfectMoneyPayment(models.Model):
+    transaction = models.OneToOneField(
+        to=Transaction,
+        on_delete=models.CASCADE,
+        verbose_name=_("related transaction"),
+        related_name="perfectmoney"
+    )
+    evoucher = models.CharField(
+        max_length=10,
+        verbose_name="E-voucher",
+        null=True,
+        blank=True
+    )
+    activation_code = models.CharField(
+        max_length=16,
+        verbose_name="activation code",
+        null=True,
+        blank=True
+    )
+
+    def __str__(self):
+        return _("EVoucher: {}").format(self.evoucher)
+
+
+class CryptoPayment(models.Model):
+    transaction = models.OneToOneField(
+        to=Transaction,
+        on_delete=models.CASCADE,
+        verbose_name=_("related transaction"),
+        related_name="crypto"
+    )
+    from_addres = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name=_("payer wallet address")
+    )
+    order_id = models.CharField(
+        max_length=50,
+        unique=True,
+        null=True,
+        blank=True,
+        verbose_name=_("order id")
+    )
+    tx_hash = models.CharField(
+        max_length=255,
+        null=True,
+        blank=True,
+        verbose_name=_("transaction hash")
+    )
+    network = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name=_("blockchain network")
+    )
+    currency = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name=_("invoice currency")
+    )
+    payer_currency = models.CharField(
+        max_length=20,
+        null=True,
+        blank=True,
+        verbose_name=_("actuall paid currency")
+    )
+    payment_amount_coin = models.DecimalField(
+        max_digits=10,
+        decimal_places=5,
+        null=True,
+        blank=True,
+        verbose_name=_("actual pay amount crypto"),
+    )
+
+    def __str__(self):
+        return _("Coin: {}:{}").format(self.network, self.currency)
