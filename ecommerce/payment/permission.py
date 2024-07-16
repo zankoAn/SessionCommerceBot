444404d2a7ce0f3ffbeded3e7d7b3e6a5d7cf933
zankoAn <zankoan4@gmail.com>
2024-07-16 18:26:24 +0330

    Add remote addres as default to check ip

--- ecommerce/payment/permission.py
+++ ecommerce/payment/permission.py
@@ -5,5 +5,6 @@ class WhitelistIPPermission(BasePermission):
     allowed_ips = ["127.0.0.1", "91.227.144.54"]
 
     def has_permission(self, request, view):
-        ips = request.META.get("HTTP_X_FORWARDED_FOR").split(",")[0]
+        remote_adress = request.META.get("REMOTE_ADDR", "")
+        ips = request.META.get("HTTP_X_FORWARDED_FOR", remote_adress).split(",")[0]
         return ips in self.allowed_ips
