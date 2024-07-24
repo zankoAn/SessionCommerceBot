b5933927f2b56bc0e058d6b6c0db7a0d01ce844c
zankoAn <zankoan4@gmail.com>
2024-07-24 12:46:21 +0330

    Add static file dirs and static root

--- config/settings.py
+++ config/settings.py
@@ -94,6 +94,8 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 STATIC_URL = "/static/"
+STATIC_ROOT = BASE_DIR / "static_root"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
